c95f99f7c52d19bd89aa428822766e0f08a36180
vinay <ghatevinay2@gmail.com>
2025-07-21 02:12:34 +0530

    Solving Werkzeug web server

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
         app,
         host='0.0.0.0',
         port=5000,
-        debug=app.config['DEBUG'],
-        use_reloader=app.config['DEBUG'],
+        # debug=app.config['DEBUG'],
+        # use_reloader=app.config['DEBUG'],
         allow_unsafe_werkzeug=True
     )
 
